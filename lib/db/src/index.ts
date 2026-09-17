fae0f317d8d733294359dbb6ad43f11c876a4f87
rizkaajengt-gif <rizkaajengt@staf.poltekkestasikmalaya.ac.id>
2026-09-17 02:55:26 +0000

    index

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -14,3 +14,15 @@ export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
 
 export * from "./schema";
+export * from "./alat";
+export * from "./bahan";
+export * from "./jurusan";
+export * from "./konten";
+export * from "./laboratorium";
+export * from "./notifikasi";
+export * from "./peminjaman";
+export * from "./permintaan";
+export * from "./plp-laboratorium";
+export * from "./relations";
+export * from "./transfer-bahan";
+export * from "./users";
